41ccb4339a82faf6b6bab42c8c70b46e643d9702
Dominique Parisot <dominique@gilbraltaz.fr>
2026-07-14 08:08:19 +0200

    Renommage et déplacement de elementTypeHandler

--- core/src/typeHandlers/elementTypeElement.ts
+++ core/src/typeHandlers/elementTypeElement.ts
@@ -3,9 +3,24 @@
  * Copyright (C) 2026 Executive Gibraltaz
  */
 
-import { ElementPath } from "@/Element";
+import { ElementData, ElementName, ElementPath } from "@/Element";
 import { elementTypeName, rootName, rootTypeContainerName, typeElementName } from "@/global";
-import { ElementTypeHandler } from "./ElementTypeHandler";
+import { FactoryFunction, TypeHandler } from "./TypeHandler";
+
+const elementFactory: FactoryFunction = (
+  _elementName: ElementName,
+  _parentPath: ElementPath,
+  _params:Record<string, any>
+): ElementData => {
+  throw new Error("Element type can not be instanciate");
+};
+
+
+const elementTypeHandler: TypeHandler = {
+  isContainer: false,
+  factory: elementFactory 
+} as TypeHandler;
+
 
 // element «#/types/element»
 const elementTypeElement = {
@@ -14,7 +29,7 @@ const elementTypeElement = {
   elementType: [rootName, rootTypeContainerName, typeElementName] as ElementPath,
   isContainer: false,
   data: {
-    typeHandler: ElementTypeHandler
+    typeHandler: elementTypeHandler
   }
 };
 
