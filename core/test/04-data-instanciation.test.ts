f6ec36df39450a9ac3aa9a6b859a6e80ce2ac663
Dominique Parisot <dominique@gilbraltaz.fr>
2026-07-17 16:55:20 +0200

    Réactivation autotests désactivés

--- core/test/04-data-instanciation.test.ts
+++ core/test/04-data-instanciation.test.ts
@@ -39,70 +39,70 @@ describe("Maestrozo core", () => {
     expect(component).to.have.property('childNames', null);
   });
 
-  //it("should find newly created component in runtime", async () => {
-  //  const component = await engine.getElement(['#' as ElementName, 'runtime' as ElementName, 'constantA' as ElementName]);
-  //  expect(component).to.be.instanceof(Object);
-  //  expect(component).to.have.property('revision', 1);
-  //  expect(component).to.have.property('elementName', 'constantA');
-  //  expect(component).to.have.property('parentPath');
-  //  expect(component.parentPath).to.deep.equal(['#', 'runtime']);
-  //  expect(component).to.have.property('elementType');
-  //  expect(component.elementType).to.deep.equal([ '#', 'types', 'components', 'constant']);
-  //  expect(component).to.have.property('isContainer', false);
-  //  expect(component).to.have.property('isVolatile', false);
-  //  expect(component).to.have.property('data');
-  //  expect(component.data).to.be.instanceof(Object);
-  //  expect(component.data).to.have.property('value', 123);
-  //  expect(component).to.have.property('childNames', null);
-  //});
+  it("should find newly created component in runtime", async () => {
+    const component = await engine.getElement(['#' as ElementName, 'runtime' as ElementName, 'constantA' as ElementName]);
+    expect(component).to.be.instanceof(Object);
+    expect(component).to.have.property('revision', 1);
+    expect(component).to.have.property('elementName', 'constantA');
+    expect(component).to.have.property('parentPath');
+    expect(component.parentPath).to.deep.equal(['#', 'runtime']);
+    expect(component).to.have.property('elementType');
+    expect(component.elementType).to.deep.equal([ '#', 'types', 'components', 'constant']);
+    expect(component).to.have.property('isContainer', false);
+    expect(component).to.have.property('isVolatile', false);
+    expect(component).to.have.property('data');
+    expect(component.data).to.be.instanceof(Object);
+    expect(component.data).to.have.property('value', 123);
+    expect(component).to.have.property('childNames', null);
+  });
 
-  //it("should find newly created component as a child of runtime", async () => {
-  //  const runtimeContainer = await engine.getElement(['#' as ElementName, 'runtime' as ElementName]);
-  //  expect(runtimeContainer).to.have.property('childNames');
-  //  expect(runtimeContainer.childNames).to.deep.equal(['constantA']);
-  //});
+  it("should find newly created component as a child of runtime", async () => {
+    const runtimeContainer = await engine.getElement(['#' as ElementName, 'runtime' as ElementName]);
+    expect(runtimeContainer).to.have.property('childNames');
+    expect(runtimeContainer.childNames).to.deep.equal(['constantA']);
+  });
 
-  //it("should not create a component with the same name", async () => {
-  //  expect( async () => {
-  //    await engine.createElement(
-  //      'constantA' as ElementName,
-  //      [ '#', 'runtime' ] as ElementPath,
-  //      [ '#', 'types', 'components', 'constant' ] as ElementPath,
-  //      {
-  //        'dataType': [ '#', 'types', 'data', 'integer' ],
-  //        'value': 123
-  //      }
-  //    );
-  //  }).to.throw("Element «/#/runtime/constantA» already exists");
-  //});
+  it("should not create a component with the same name", async () => {
+    await expect(
+      engine.createElement(
+        'constantA' as ElementName,
+        [ '#', 'runtime' ] as ElementPath,
+        [ '#', 'types', 'components', 'constant' ] as ElementPath,
+        {
+          'dataType': [ '#', 'types', 'data', 'integer' ],
+          'value': 123
+        }
+      )
+    ).rejects.toThrow("Element «/#/runtime/constantA» already exists");
+  });
 
-  //it("should not create a component in a non-existent container", async () => {
-  //  expect( async () => {
-  //    await engine.createElement(
-  //      'constantX' as ElementName,
-  //      [ '#', 'runtime', 'xxx' ] as ElementPath,
-  //      [ '#', 'types', 'components', 'constant' ] as ElementPath,
-  //      {
-  //        'dataType': [ '#', 'types', 'data', 'integer' ],
-  //        'value': 123
-  //      }
-  //    );
-  //  }).to.throw("Parent element «/#/runtime/xxx» does not exist");
-  //});
+  it("should not create a component in a non-existent container", async () => {
+    await expect(
+      engine.createElement(
+        'constantX' as ElementName,
+        [ '#', 'runtime', 'xxx' ] as ElementPath,
+        [ '#', 'types', 'components', 'constant' ] as ElementPath,
+        {
+          'dataType': [ '#', 'types', 'data', 'integer' ],
+          'value': 123
+        }
+      )
+    ).rejects.toThrow("Parent element «/#/runtime/xxx» does not exist");
+  });
 
 
-  //it("should not create a component in non-container component", async () => {
-  //  expect( async () => {
-  //    await engine.createElement(
-  //      'constantX' as ElementName,
-  //      [ '#', 'runtime', 'constantA' ] as ElementPath,
-  //      [ '#', 'types', 'components', 'constant' ] as ElementPath,
-  //      {
-  //        'dataType': [ '#', 'types', 'data', 'integer' ],
-  //        'value': 123
-  //      }
-  //    );
-  //  }).to.throw("Parent element «/#/runtime/constantA» is not a container");
-  //});
+  it("should not create a component in non-container component", async () => {
+    await expect(
+      engine.createElement(
+        'constantX' as ElementName,
+        [ '#', 'runtime', 'constantA' ] as ElementPath,
+        [ '#', 'types', 'components', 'constant' ] as ElementPath,
+        {
+          'dataType': [ '#', 'types', 'data', 'integer' ],
+          'value': 123
+        }
+      )
+    ).rejects.toThrow("Parent element «/#/runtime/constantA» is not a container");
+  });
 
 });
